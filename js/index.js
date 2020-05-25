7a138f145d494bf9c698f16cbca08d0ec699028f
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2020-05-25 15:02:30 +0200

    MQTT.js geimporteerd en verbinding gelegd
    MQTT.js geimporteerd en koppeling gelegd naar MQTT server in MQTT.js

--- js/index.js
+++ js/index.js
@@ -55,5 +55,38 @@ firebase.auth().onAuthStateChanged(function(user) {
   }).catch(function(error) {
     window.alert("Er is iets foutgegaan bij het uitloggen");
   });
+ }
 
-}
+// var user = firebase.auth().currentUser;
+// var name, email, photoUrl, uid, emailVerified;
+
+// if (user != null) {
+//   name = user.displayName;
+//   email = user.email;
+//   photoUrl = user.photoURL;
+//   emailVerified = user.emailVerified;
+//   uid = user.uid; 
+// }
+
+// var user = firebase.auth().currentUser;
+
+// if (user != null) {
+//   user.providerData.forEach(function (profile) {
+//     console.log("Sign-in provider: " + profile.providerId);
+//     console.log("  Provider-specific UID: " + profile.uid);
+//     console.log("  Name: " + profile.displayName);
+//     console.log("  Email: " + profile.email);
+//     console.log("  Photo URL: " + profile.photoURL);
+//   });
+// }
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('broker.mqtt-dashboard.com', {
+    username: 'drip',
+    password: 'drip123'
+});
+
+client.on('connect', function() {
+    console.log('connected');
+});
+ 
---
+++ mqtt.js
@@ -0,0 +1,13 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://broker.mqtt-dashboard.com')
+var topic = 'sjoerdMessage'
+
+client.on('message', (topic, message)=>{
+    message = message.toString()
+    console.log(message);
+})
+
+client.on('connect', ()=> {
+    client.subscribe(topic);
+     
+})
